8d30dd3897df50bd27920da7b32062c5fb197353
Brian <brian12leonard@gmail.com>
2025-02-09 14:03:41 -0800

    initial commit

---
+++ SocketsExample/SocketClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketsPractice
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hostname = Dns.GetHostName();
+            IPHostEntry localhost = await Dns.GetHostEntryAsync(hostname);
+            IPAddress localIpAddress = localhost.AddressList[0];
+            IPEndPoint endPoint = new(localIpAddress, 11_000);
+
+            using Socket client = new(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            await client.ConnectAsync(endPoint);
+            var receiveTask = ReceiveMessagesAsync(client); // Start receiving messages in parallel.
+
+            while (true)
+            {
+                // Send message
+                var message = Console.ReadLine();
+                if (message == null) break;
+
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                await client.SendAsync(messageBytes, SocketFlags.None);
+                Console.WriteLine($"Socket client sent message: \"{message}\"");
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+        }
+
+        static async Task ReceiveMessagesAsync(Socket client)
+        {
+            var buffer = new byte[1024];
+            while (true)
+            {
+                try
+                {
+                    // Receive message asynchronously
+                    var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                    if (received == 0) break;
+
+                    var response = Encoding.UTF8.GetString(buffer, 0, received);
+                    Console.WriteLine($"Socket client received message: \"{response}\"");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error receiving message: {ex.Message}");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ SocketsExample/SocketServer/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    internal class Program
+    {
+        private static readonly List<Socket> _clients = new List<Socket>();
+
+        static async Task Main(string[] args)
+        {
+            var hostname = Dns.GetHostName();
+            IPHostEntry localhost = await Dns.GetHostEntryAsync(hostname);
+            IPAddress localIpAddress = localhost.AddressList[0];
+            IPEndPoint endPoint = new(localIpAddress, 11_000);
+
+            // Create listener
+            using Socket listener = new(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind endpoint to listener
+            listener.Bind(endPoint);
+            listener.Listen(10);
+
+            while (true)
+            {
+                var handler = await listener.AcceptAsync(); // Accept incoming request
+                _clients.Add(handler);
+                _ = HandleClientCommunication(handler);
+            }
+        }
+
+        static async Task HandleClientCommunication(Socket handler)
+        {
+            try
+            {
+                while (true)
+                {
+                    var buffer = new byte[1024];
+                    int received;
+
+                    try
+                    {
+                        // Receive message
+                        received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+
+                        if (received == 0)
+                        {
+                            // Client has disconnected, stop the loop
+                            break;
+                        }
+
+                        var response = Encoding.UTF8.GetString(buffer, 0, received);
+                        Console.WriteLine($"Server received message: \"{response}\"");
+
+                        // Broadcast message to all other clients
+                        foreach (var client in _clients)
+                        {
+                            Console.WriteLine($"Comparing client: {client} with handler: {handler}");
+                            if (client != handler && client.Connected)
+                            {
+                                var messageBytes = Encoding.UTF8.GetBytes(response);
+                                await client.SendAsync(messageBytes, SocketFlags.None);
+                                Console.WriteLine($"Sent message to client: \"{response}\"");
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error receiving message: {ex.Message}");
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error handling client communication: {ex.Message}");
+            }
+            finally
+            {
+                // Clean up
+                _clients.Remove(handler);
+                handler.Close();
+                Console.WriteLine("Client disconnected and removed from list.");
+            }
+        }
+    }
+}
